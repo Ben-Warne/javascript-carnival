1e0e9f55d74f9791dfcaf154942cb168d0263dce
Ben warne <benwarne96@gmail.com>
2022-05-11 15:11:49 +1200

    finished unicorn challenge

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,56 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+console.log('Inflate The Unicorn!')
+//create onlick function for unicorns
+let corns = document.getElementsByClassName('inflate-an-image')
+
+for (let i = 0; i < corns.length; i++) {
+  corns[i].onclick = cornsClick
+}
+
+let balloonInflate = 0
+//change unicorn image when clicked upon
+function cornsClick(e) {
+  let corns = e.target
+
+  if (corns.id == 'unicorn1') {
+    balloonInflate++
+
+    corns.src = './images/unicorn-' + balloonInflate + '.png'
+    console.log(corns)
+
+    if (balloonInflate > 3) {
+      balloonInflate = 0
+      corns.src = './images/unicorn-' + balloonInflate + '.png'
+      console.log('thanks')
+    }
+  }
+
+  if (corns.id == 'unicorn-2') {
+    balloonInflate++
+
+    corns.src = './images/unicorn-' + balloonInflate + '.png'
+    console.log(corns)
+
+    if (balloonInflate > 3) {
+      balloonInflate = 0
+      corns.src = './images/unicorn-' + balloonInflate + '.png'
+      console.log('thanks')
+    }
+  }
+  console.log('click me again!')
+  if (corns.id == 'unicorn-3') {
+    balloonInflate++
+
+    corns.src = './images/unicorn-' + balloonInflate + '.png'
+    console.log(corns)
+
+    if (balloonInflate > 3) {
+      balloonInflate = 0
+      corns.src = './images/unicorn-' + balloonInflate + '.png'
+      console.log('thanks')
+    }
+  }
+}
