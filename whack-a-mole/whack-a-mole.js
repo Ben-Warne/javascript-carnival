f4cc1f90ce09833dd7ff1f96c49897e37952d67f
Ben warne <benwarne96@gmail.com>
2022-05-12 16:07:46 +1200

    finished mole challenge

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,27 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+console.log('Whack-a-Mole!')
+//Make click function
+let cells = document.getElementsByTagName('td')
+let moleImage = document.createElement('img')
+moleImage.classList.add('mole')
+moleImage.src = 'mole.PNG'
+moleImage.onclick = whackedMole
+
+//Make random mole generator
+function moleHome() {
+  let randomIndex = Math.floor(Math.random() * 24)
+  let randomCell = cells[randomIndex]
+
+  randomCell.appendChild(moleImage)
+}
+moleHome()
+
+//remove mole after click
+function whackedMole() {
+  console.log('Boing!')
+  moleHome()
+  new Audio('whack-audio.wav').play()
+}
