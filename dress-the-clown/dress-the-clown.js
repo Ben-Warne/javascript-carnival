6d51800ab1a90984af85bcc6230c75f3df701112
Ben warne <benwarne96@gmail.com>
2022-05-13 15:52:05 +1200

    tried my best with the clown

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -1,5 +1,31 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Dress The Clown!")
+console.log('Dress The Clown!')
+
+let headIndex = 0
+
+document.addEventListener('keydown', function (e) {
+  switch (e.key) {
+    case e.key:
+      changeClownHead()
+      changeClothes()
+      break
+  }
+})
+//function for changing the head
+//make option for multiple heads
+function changeClownHead() {
+  console.log(changeClownHead)
+  let head = document.getElementById('head')
+  let headSrc = './images/head' + headIndex + '.png'
+}
+
+function changeClothes() {
+  if (headIndex > 5) {
+    headIndex = 0
+  } else if (headIndex < 0) {
+    headIndex = 5
+  }
+}
